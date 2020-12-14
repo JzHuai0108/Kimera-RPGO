a5f3ffcdee9f64890692de3843b6ea8a4a80e5ad
JIanzhu Huai <huai.3@osu.edu>
2020-12-14 17:57:27 +0800

    upgrade gtsam

--- KimeraRPGO/utils/geometry_utils.h
+++ KimeraRPGO/utils/geometry_utils.h
@@ -37,7 +37,7 @@ static size_t getTranslationDim() {
   // get translation dimension of some gtsam object
   BOOST_CONCEPT_ASSERT((gtsam::IsLieGroup<T>));
   T sample_object;
-  return sample_object.translation().dimension;
+  return sample_object.translation().size();
 }
 
 template <class T>
@@ -91,7 +91,7 @@ struct PoseWithCovariance {
     pose = between_factor.measured();
     gtsam::Matrix covar =
         boost::dynamic_pointer_cast<gtsam::noiseModel::Gaussian>(
-            between_factor.get_noiseModel())
+            between_factor.noiseModel())
             ->covariance();
 
     // prevent propagation of nan values in the edge case
@@ -177,11 +177,11 @@ struct PoseWithCovariance {
       int r_dim = getRotationDim<T>();
       Eigen::MatrixXd cov_block =
           covariance_matrix.block(r_dim, r_dim, t_dim, t_dim);
-      return std::sqrt(log.tail(t_dim).transpose() * gtsam::inverse(cov_block) *
+      return std::sqrt(log.tail(t_dim).transpose() * cov_block.inverse() *
                        log.tail(t_dim));
     }
 
-    return std::sqrt(log.transpose() * gtsam::inverse(covariance_matrix) * log);
+    return std::sqrt(log.transpose() * covariance_matrix.inverse() * log);
   }
 };
 
@@ -221,7 +221,7 @@ struct PoseWithNode {
     pose = between_factor.measured();
 //    gtsam::Matrix covar =
 //        boost::dynamic_pointer_cast<gtsam::noiseModel::Gaussian>(
-//            between_factor.get_noiseModel())
+//            between_factor.noiseModel())
 //            ->covariance();
 
     // prevent propagation of nan values in the edge case
